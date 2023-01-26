d4d04f192009b60ec4cfc1d6af9d5086ad57be69
melwhite1 <melissawhite668@gmail.com>
2023-01-26 11:44:25 -0700

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://intense-river-67514.herokuapp.com/']
+ALLOWED_HOSTS = ['intense-river-67514.herokuapp']
 
 
 # Application definition
